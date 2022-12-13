a345f549dcdb389b8e9fa7f5d8494112769d4775
Thiman <thimankrishh@gmail.com>
2022-12-13 20:58:16 +0000

    add Ws paths for Comments resources

--- src/main/java/com/springboot/blog/common/WsPath.java
+++ src/main/java/com/springboot/blog/common/WsPath.java
@@ -6,6 +6,7 @@ public interface WsPath {
     /** API version 1 */
     String API_V_1 = "/api/1";
 
+    /** ############################## POST DATA RESOURCES ##########################*/
     String POSTS = API_V_1 + "/posts";
 
     /**Ws path to all posts data**/
@@ -19,4 +20,22 @@ public interface WsPath {
 
     /**Ws path to Delete post **/
     String DELETE_POST = POSTS + "/{id}";
+
+    /** ############################## COMMENT DATA RESOURCES ##########################*/
+
+
+    /**Ws path to get all comments of a post **/
+    String COMMENTS = POSTS + "{postId}/comments";
+
+    /**Ws path to get comment of a post by comment Id **/
+    String GET_COMMENT = COMMENTS + "/{id}";
+
+    /**Ws path to create a new comment on a post **/
+    String CREATE_COMMENT = COMMENTS;
+
+    /**Ws path to update a comment of a post **/
+    String UPDATE_COMMENT = COMMENTS + "/{id}";
+
+    /**Ws path to delete a comment of a post **/
+    String DELETE_COMMENT = COMMENTS + "/{id}";
 }
