b051a5f80167299aeac82d94d6430a2240f8be29
Thiman <thimankrishh@gmail.com>
2022-12-13 17:37:07 +0000

    add rest end point methods in post controller

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -6,9 +6,9 @@ import com.springboot.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PostController {
@@ -23,5 +23,33 @@ public class PostController {
 
         return new ResponseEntity<>(postService.createPost(post), HttpStatus.CREATED);
 
+    }
+
+    @GetMapping(WsPath.GET_POST)
+    public ResponseEntity<PostDto> getPostById(@PathVariable Long id){
+
+        return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
+    }
+
+    @PutMapping(WsPath.UPDATE_POST)
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") Long id){
+
+        PostDto postResponse = postService.updatePost(postDto, id);
+
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping(WsPath.DELETE_POST)
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+
+        postService.deletePostById(id);
+
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
+
+    @GetMapping(WsPath.POSTS)
+    public List<PostDto> getAllPosts(){
+
+        return postService.getAllPosts();
     }
 }
