8a40bd05b087b0374b221e60806cf0f53fc9d1ed
Thiman <thimankrishh@gmail.com>
2022-12-14 21:23:46 +0000

    add data.sql file to load initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO posts VALUES (1, 'Java for beginners', 'Blog post about java for beginners', 'Java is a popular programming language'),
+                         (2, 'React for beginners', 'Blog post about react js for beginners', 'React is a popular front-end library'),
+                         (3, 'Search Engine Optimization', 'Blog post about SEO', 'SEO is essential technique for web-development');
+
+INSERT INTO comments VALUES (1, 'Jane Smith', 'js@gmail.com', 'Useful blog!', 1),
+                            (2, 'Niel Parker', 'np12@gmail.com', 'Good for beginners', 1),
+                            (3, 'Dan J', 'dj@gmail.com', 'Great post', 2);
